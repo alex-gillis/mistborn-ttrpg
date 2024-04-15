ab1dd91b5a0153c88c66a3f7e17ace38f1754ce8
alex-gillis <alex.s.gillis@gmail.com>
2024-04-15 04:45:30 -0300

    Correct JSON

--- src/views/Metallurgy/Metal.jsx
+++ src/views/Metallurgy/Metal.jsx
@@ -4,21 +4,20 @@ import Hemalurgy from "./Hemalurgy"
 
 /* eslint-disable react/prop-types */
 function Metal(props) {
-
-    return (
-      < >
-        <h2>{ props.metal.name }</h2>
-        <h4>{ props.metal.category } / { props.metal.firstQuality } / { props.metal.secondQuality }</h4>
-        { props.metal.rarity }
-        <br/>
-        <Allomancy metal={ props.metal }/>
-        <br/>
-        <Feruchemy metal={ props.metal }/>
-        <br/>
-        <Hemalurgy metal={ props.metal }/>
-      </>
-    )
-  }
+  return (
+    < >
+      <h2>{ props.metal.name }</h2>
+      <h4>{ props.metal.category } / { props.metal.firstQuality } / { props.metal.secondQuality }</h4>
+      { props.metal.rarity }
+      <br/>
+      <Allomancy metal={ props.metal }/>
+      <br/>
+      <Feruchemy metal={ props.metal }/>
+      <br/>
+      <Hemalurgy metal={ props.metal }/>
+    </>
+  )
+}
   
   export default Metal
   
