1fc4f0ea0ffe080172de7fe848bdf974641a5313
alex-gillis <alex.s.gillis@gmail.com>
2024-04-17 19:47:21 -0300

    Feruchemical Gunplay Stunts

---
+++ src/views/Metallurgy/Feruchemy/Gunplay.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+function Gunplay(props) {
+    let myGunplay = props.metal.allomancy.gunplay;
+    if (myGunplay !== undefined && myGunplay.length >= 1) {
+        return (
+          < >
+            <h4>Feruchemical Gunplay Stunts with { props.metal.name }</h4>
+            <div>
+              Just as with Allomancers, Ferrings have learned to combine their Feruchemy with their shooting skills to get results impossible for ordinary humans to duplicate. These special talents and tricks are known as <span id="bold">Feruchemical Shot Stunts</span>
+              <br/>
+              <span id="bold">Any Ferr or Twinborn Hero</span> may gain Feruchemical shot Stunts corresponding to his or her metals during character creation, or purchase them during play using Advancements.
+            </div>
+            {myGunplay?.map((myGun, index) => (
+                <div key={index}>   
+                    <span id="bolder">{myGun.name}: </span>{myGun.description}
+                    <br/>
+                </div>
+            ))}
+          </>
+        )
+    } else {
+        return (
+            < ></>
+          )
+    } 
+  }
+  
+  export default Gunplay
+  
