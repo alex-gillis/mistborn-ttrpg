e72811c1234d9bfc2dd7dfb41e5c93dc74cf0901
alex-gillis <alex.s.gillis@gmail.com>
2024-04-17 18:20:25 -0300

    GitHub Pages Fix 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mistborn-ttrpg/', // Adjust this to match your repository name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory for the production build
+    assetsDir: '.', // Assets directory relative to outDir
+    sourcemap: false, // Disable sourcemaps for production build
+  },
+});
